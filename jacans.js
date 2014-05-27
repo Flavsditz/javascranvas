a0b133db7df50a33d6619c4a471c292fd07b846e
FDiez <flavio.diez@infomotion.de>
2014-05-27 14:57:37 +0200

    new support files added deleted old support files implemented basic init structure

--- jacans.js
+++ jacans.js
@@ -2,47 +2,124 @@
  * JAvasCrANvaS (JACANS): A plugin for JQuery that manipulates and draws on the HTML5 Canvas Element
  * @author Flavio Diez
  */
- 
- $( document ).ready(function(){
-	var canvas = document.getElementById("canvas_1");
-	if (canvas.getContext){
-		var canvas_context = canvas.getContext("2d");
+
+/*******************************************************************************
+ * GLOBALS
+ ******************************************************************************/
+var lineChoice = ""
+var toolChoice = ""
+
+$(document).ready(function() {
+
+	/*
+	 * Get canvas and context
+	 */
+	var canvas = $("#mainCanvas")[0];
+	var canvas_context;
+
+	if (!canvas) {
+		console.log('Error: I cannot find the canvas element!');
+		// alert('Error: I cannot find the canvas element!');
+		return;
 	}
- 
-	$("#canvas_1").on("click", function(event){
+
+	if (!canvas.getContext) {
+		console.log('Error: no canvas.getContext!');
+		// alert('Error: no canvas.getContext!');
+		return;
+	} else {
+		canvas_context = canvas.getContext("2d");
+	}
+
+	if (!canvas_context) {
+		console.log('Error: failed to getContext!');
+		// alert('Error: failed to getContext!');
+		return;
+	}
+
+	/*
+	 * Create a temporary canvas to be drawn upon
+	 */
+	var tmpCanvas = document.createElement('canvas');
+
+	}
+
+	tmpCanvas.id = 'tmpCanvas';
+	tmpCanvas.width = canvas.width;
+	tmpCanvas.height = canvas.height;
+	$("#mainCanvas").parent().append(tmpCanvas);
+
+	var tmpContext = canvas.getContext('2d');
+
+	/*
+	 * Set up listeners
+	 */
+	
+	// Canvas
+	$("#mainCanvas").on("mousedown", function(event) {
 		drawHorizontal(event, canvas, canvas_context);
 	});
-	
-	
- });
- 
- function drawHorizontal(event, canvas, ctx){
-	mouse = getMousePosition(event);
-	ctx.moveTo(mouse[0], mouse[1]);
-	ctx.lineTo(canvas.height, mouse[1]);
-	ctx.stroke();
- }
- 
- function drawVertical(event, canvas, ctx){
-	mouse = getMousePosition(event);
-	ctx.moveTo(mouse[0], mouse[1]);
-	ctx.lineTo(mouse[0], canvas.width);
-	ctx.stroke();
- }
- 
- // Helper function
- function getMousePosition(e)
-{
-    var mouseX, mouseY;
+	$("#mainCanvas").on("mousemove", function(event) {
+		drawHorizontal(event, canvas, canvas_context);
+	});
+	$("#mainCanvas").on("mouseup", function(event) {
+		drawHorizontal(event, canvas, canvas_context);
+	});
 
-    if(e.offsetX) {
-        mouseX = e.offsetX;
-        mouseY = e.offsetY;
-    }
-    else if(e.layerX) {
-        mouseX = e.layerX;
-        mouseY = e.layerY;
-    }
+	// Buttons
+	$("#clearButton").on("click", function(event) {
+		clearCanvas(canvas, canvas_context);
+	});
+	$("#horizButton").on("click", function(event) {
+		$(this).toggleClass("active");
+	});
+	$("#vertButton").on("click", function(event) {
+		$(this).toggleClass("active");
+	});
+	
+	//
+});
 
-    return [mouseX, mouseY]
+function drawHorizontal(event, canvas, ctx) {
+	mouse = getMousePosition(event);
+	ctx.beginPath();
+	ctx.moveTo(0, mouse[1]);
+	ctx.lineTo(canvas.width, mouse[1]);
+	ctx.stroke();
+}
+
+function drawVertical(event, canvas, ctx) {
+	mouse = getMousePosition(event);
+	ctx.moveTo(mouse[0], 0);
+	ctx.lineTo(mouse[0], canvas.height);
+	ctx.stroke();
+}
+
+/*******************************************************************************
+ * Helper functions
+ ******************************************************************************/
+function getMousePosition(e) {
+	var mouseX, mouseY;
+
+	if (e.offsetX) {
+		mouseX = e.offsetX;
+		mouseY = e.offsetY;
+	} else if (e.layerX) {
+		mouseX = e.layerX;
+		mouseY = e.layerY;
+	}
+
+	return [ mouseX, mouseY ]
+}
+
+// Clear canvas of any line
+function clearCanvas(canvas, ctx) {
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+
+// Update the image
+function img_update (ctx_main, ctx_tmp, canvas) {
+	ctx_main.drawImage(canvas, 0, 0);
+	ctx_tmp.clearRect(0, 0, canvas.width, canvas.height);
 }
