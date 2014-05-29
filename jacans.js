77b59a3a6580c4a04f40927fea8dc3c3055403b9
Flavio Diez <flaviofdiez@gmail.com>
2014-05-29 10:46:12 +0200

    added brain fcn and new way to deal with the  tools

--- jacans.js
+++ jacans.js
@@ -6,8 +6,6 @@
 /*******************************************************************************
  * GLOBALS
  ******************************************************************************/
-var lineChoice = ""
-var toolChoice = ""
 
 $(document).ready(function() {
 
@@ -80,7 +78,15 @@ $(document).ready(function() {
 	//
 });
 
-function brain()
+function brain(event){
+	mouse = getMousePosition(event);
+	line = $('.tool .active').attr('name');
+	
+	if(tools[line]){
+		var func = line[event.type];
+		func(event);
+	}
+}
 
 function drawHorizontal(event, canvas, ctx) {
 	mouse = getMousePosition(event);
@@ -96,6 +102,43 @@ function drawVertical(event, canvas, ctx) {
 	ctx.lineTo(mouse[0], canvas.height);
 	ctx.stroke();
 }
+
+  // This object holds the implementation of each drawing tool.
+  var tools = {};
+
+  // The drawing pencil.
+  tools.horizontalLine = function () {
+    var tool = this;
+    this.started = false;
+
+    // This is called when you start holding down the mouse button.
+    // This starts the pencil drawing.
+    this.mousedown = function (ev) {
+        context.beginPath();
+        context.moveTo(ev._x, ev._y);
+        tool.started = true;
+    };
+
+    // This function is called every time you move the mouse. Obviously, it only 
+    // draws if the tool.started state is set to true (when you are holding down 
+    // the mouse button).
+    this.mousemove = function (ev) {
+      if (tool.started) {
+        context.lineTo(ev._x, ev._y);
+        context.stroke();
+      }
+    };
+
+    // This is called when you release the mouse button.
+    this.mouseup = function (ev) {
+      if (tool.started) {
+        tool.mousemove(ev);
+        tool.started = false;
+        img_update();
+      }
+    };
+  };
+
 
 /*******************************************************************************
  * Helper functions
